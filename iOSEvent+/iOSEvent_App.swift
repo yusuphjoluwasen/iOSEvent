3ba29c8311cf9907026d8394477e7f1d6dec5639
Guru <yusuphjoluwaseun@gmail.com>
2023-03-02 06:48:17 +0000

    Initial Commit

---
+++ iOSEvent+/iOSEvent_App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSEvent_App.swift
+//  iOSEvent+
+//
+//  Created by Guru King on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOSEvent_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
